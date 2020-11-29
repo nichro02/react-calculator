50f0896f0a98ef44d6ad285295e89c3c8a50d641
nichro02 <nichro02@gmail.com>
2020-11-28 18:30:45 -0800

    Refactored code to save operator to be used in calculator and added method for mathematical operation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,11 +6,13 @@ class Calculator extends Component {
     state = {
         count: 0,
         num1: 0,
-        num2: 0
+        num2: 0,
+        operation: ''
     }
 
     updateNum1 = (event) => {
         console.log('UPDATE num1 to', event.target.value)
+        
 
         this.setState({
             num1: event.target.value
@@ -25,17 +27,59 @@ class Calculator extends Component {
         })
     }
 
-    add = (event) => {
+    operator = (event) => {
         event.preventDefault()
         console.log('ADD FUNCTION FIRED')
+        console.log('SET OPERATION TO --->',event.target.innerHTML)
 
         this.setState({
-            count: parseInt(this.state.num1) + parseInt(this.state.num2)
+            //count: parseInt(this.state.num1) + parseInt(this.state.num2),
+            operation: event.target.innerHTML
+        })
+    }
+
+    subtract = (event) => {
+        event.preventDefault()
+        console.log('SUBTRACT FUNCTION FIRED')
+
+        this.setState({
+            count: parseInt(this.state.num1) - parseInt(this.state.num2)
+        })
+    }
+
+    mathOperation = (event) => {
+        event.preventDefault()
+        console.log('OPERATOR FIRED --->', this)
+
+        if (this.state.operation === '+') {
+            this.setState({
+                count: parseInt(this.state.num1) + parseInt(this.state.num2)
+            })
+        } else if (this.state.operation === '-') {
+            this.setState({
+                count: parseInt(this.state.num1) - parseInt(this.state.num2)
+            })
+        } else if (this.state.operation === '*') {
+            this.setState({
+                count: parseInt(this.state.num1) * parseInt(this.state.num2)
+            })
+        } else if (this.state.operation === '/') {
+            this.setState({
+                count: parseInt(this.state.num1) / parseInt(this.state.num2)
+            })
+        }
+    }
+
+    reset = (event) => {
+        this.setState({
+            count: 0,
+            num1: 0,
+            num2: 0,
+            operation: ''
         })
     }
 
     render(){
-        
 
         return (
             <div className='container'>
@@ -47,16 +91,21 @@ class Calculator extends Component {
                         onChange={(event) => this.updateNum1(event)}
                     />
                     
-                    <button onClick={this.add}> + </button>
+                    <button onClick={this.operator}>+</button>
+                    <button onClick={this.operator}>-</button>
+                    <button onClick={this.operator}>*</button>
+                    <button onClick={this.operator}>/</button>
 
                     <input type='text' 
                         placeholder='0'
                         onChange={(event) => this.updateNum2(event)}
                     />
 
-                    <span> = </span>
+                    <button onClick={this.mathOperation}> = </button>
 
                     <h3>{this.state.count}</h3>
+
+                    <button onClick={this.reset}>Clear</button>
                 </div>
             </div>
       )
